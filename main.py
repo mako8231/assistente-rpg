55a8f0b8577a461b6355207918df2da94632629a
masao8231 <mm.neto2007@gmail.com>
2021-10-09 18:43:35 -0300

    removido token

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ class MyClient(discord.Client):
         print('Message from {0.author}: {0.content}'.format(message))
 
 client = MyClient()
-client.run('ODg3ODMzNzExODAwOTAxNjMy.YUJ5lA.EU8brwW6DNbBACXbcLgEU2VRJ2g')    
+client.run('TOKEN')    
