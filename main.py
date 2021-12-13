e896214474747f59d0e3d185138de7776f821c71
Masao <mm.neto2007@gmail.com>
2021-12-13 11:36:59 -0400

    criação do arquivo de configuração

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+from os import close
 import discord
+import os.path
+import json
+
+filename = "config.json"
+
+#Verificar se o arquivo de configuração existe  
+if not os.path.isfile(filename):
+    print("O arquivo não existe, criando uma nova configuração na pasta do programa:")
+    with open(filename, "w") as nf:
+        token = input("Insira o token do seu bot")
+        nf.write('{"token":"'+token+'"}')
+
+
+#se o arquivo existe, leia 
+file = open(filename, "r")
+conf_str = file.readlines()
+json_parsed = json.loads(conf_str[0])
+
+cli = json_parsed["token"]
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -8,4 +29,4 @@ class MyClient(discord.Client):
         print('Message from {0.author}: {0.content}'.format(message))
 
 client = MyClient()
-client.run('TOKEN')    
+client.run(cli)    
